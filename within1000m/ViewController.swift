d4716342ec24b5ec4f4bb24a303c2860edea7946
Xu Yonhom <add@getpocket.com>
2018-03-08 11:29:08 +0800

    Initial Commit

---
+++ within1000m/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  within1000m
+//
+//  Created by 徐永宏 on 2018/3/8.
+//  Copyright © 2018年 徐永宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
